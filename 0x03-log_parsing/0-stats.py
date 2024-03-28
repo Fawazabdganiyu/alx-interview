a0dd58c5a7a9763a4e8cd8c75d46452514505431
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-03-28 18:32:00 +0100

    fix: refactor the input line count and the keyboard interrupt possition

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,11 +5,20 @@ import re
 import sys
 
 
-count = 0
-file_size = 0
-status_code = defaultdict(int)
-for line in sys.stdin:
-    line = line.strip()
+def print_metrics(current_file_size: int, current_status_codes: dict) -> None:
+    """ Print out the metrics """
+    sorted_status_code = sorted(current_status_codes)
+    print(f'File size: {current_file_size}')
+    for key in sorted_status_code:
+        print(f'{key}: {current_status_codes[key]}')
+
+
+def main():
+    count = 0
+    file_size = 0
+    possible_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+    status_codes = defaultdict(int)
+
     pattern = r'''
     ^                                     # The beginning of line
     (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})  # <IP Address>
@@ -20,17 +29,21 @@ for line in sys.stdin:
     $                                     # The end of the line
     '''
     compiled_pattern = re.compile(pattern, re.VERBOSE)
-    if compiled_pattern.match(line):
-        count += 1
-        file_size += int(line.split(' ')[-1])
-        code = line.split(' ')[-2]
-        status_code[code] += 1
-        sorted_status_code = sorted(status_code)
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            if compiled_pattern.match(line):
+                count += 1
+                file_size += int(line.split(' ')[-1])
+                code = int(line.split(' ')[-2])
+                if code in possible_codes:
+                    status_codes[code] += 1
 
-        if count == 10:
-            count = 0
-            print(f'File size: {file_size}')
-            for key in sorted_status_code:
-                print(f'{key}: {status_code[key]}')
-        if not line:
-            break
+                if not count % 10:
+                    print_metrics(file_size, status_codes)
+    except KeyboardInterrupt:
+        print_metrics(file_size, status_codes)
+
+
+if __name__ == '__main__':
+    main()
