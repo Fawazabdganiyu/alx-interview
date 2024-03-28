937dd65f7dd4dd9da265eec10865eb0f967011a2
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-03-28 20:14:42 +0100

    fix: handle where the status code is not an integer

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,19 +13,27 @@ def print_metrics(current_file_size: int, current_status_codes: dict) -> None:
         print(f'{key}: {current_status_codes[key]}')
 
 
+def is_integer(input_str):
+    try:
+        int(input_str)
+        return True
+    except ValueError:
+        return False
+
+
 def main():
     count = 0
     file_size = 0
-    possible_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+    possible_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     status_codes = defaultdict(int)
 
     pattern = r'''
     ^                                     # The beginning of line
     (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})  # <IP Address>
     \s-\s                                 # Space, hyphen and space
-    \[(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{6})\] # [<date and time>]
+    \[.+\] # [<date and time>]
     \s"GET\s/projects/260\sHTTP/1\.1"\s     # The GET request line
-    (\d{3})\s(\d{1,4})                    # The status code and file size
+    (.{3})\s(.{1,4})                    # The status code and file size
     $                                     # The end of the line
     '''
     compiled_pattern = re.compile(pattern, re.VERBOSE)
@@ -33,13 +41,13 @@ def main():
         for line in sys.stdin:
             line = line.strip()
             if compiled_pattern.match(line):
-                count += 1
-                file_size += int(line.split(' ')[-1])
-                code = int(line.split(' ')[-2])
-                if code in possible_codes:
+                code = line.split(' ')[-2]
+                if code in possible_codes and is_integer(code):
+                    file_size += int(line.split(' ')[-1])
+                    count += 1
                     status_codes[code] += 1
 
-                if not count % 10:
+                if count % 10 == 0:
                     print_metrics(file_size, status_codes)
     except KeyboardInterrupt:
         print_metrics(file_size, status_codes)
