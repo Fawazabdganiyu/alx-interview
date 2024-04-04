8d5b5888b77df5478bb826138182fc7d9d96060f
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-04-04 14:35:34 +0100

    Refactor using bit manipulation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,42 +14,20 @@ def validUTF8(data: List) -> bool:
     Returns:
         bool: True if data is a valid utf-8 encoding else False
     """
-    def count_ones(current_data: int) -> int:
-        """ Count the initial ones in the current code point
-        """
-        if current_data:
-            binary = bin(current_data)[2:]
-            count = 0
-            for bit in binary:
-                if bit == '0':
-                    break
-                else:
-                    count += 1
-            return count
-
-    def validate(data: List, start: int, end: int) -> bool:
-        """ Validate the set of code points
-        """
-        valid = True
-        for i in range(start, end):
-            binary = bin(data[i])[2:]
-            if not binary.startswith('10'):
-                valid = False
-                break
-
-        return valid
-
+    bits = 0
     valid = True
-    for i in range(len(data)):
-        ones = count_ones(data[i])
-        if not ones:
-            continue
-        if ones != 1:
-            try:
-                valid = validate(data, i + 1, ones)
-            except IndexError:
-                valid = False
-                break
-        i = ones + 1
+    for i in data:
+        lsb = 1 << 7
+        if bits == 0:
+            while i & lsb:  # 110xxxxx	10xxxxxx
+                bits += 1
+                lsb >>= 1
+            if bits == 0:
+                continue
+        else:
+            if bits < 5 and (i & lsb) and not (i & (1 << 6)):
+                bits -= 1
+            else:
+                valid =  False
 
     return valid
